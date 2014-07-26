e9255e2fd8cbc6b62d40b95ce841445855eba1be
Kris Hamoud <khamoud@umich.edu>
2014-07-26 15:57:24 -0700

    login

--- server/config/accounts.js
+++ server/config/accounts.js
@@ -1,1 +1,21 @@
 /* accounts.js = Accounts.config(), etc. */
+ServiceConfiguration.configurations.remove({
+  service: "google"
+});
+
+ServiceConfiguration.configurations.insert({
+  service: "google",
+  clientId: "1057430303583-hvkrpj9alufcib0bjjfgaf6o128l95ko.apps.googleusercontent.com",
+  secret: "hxumS37nfMXOMF2XxE7UvDCu"
+});
+
+ServiceConfiguration.configurations.remove({
+  service: "facebook"
+});
+
+ServiceConfiguration.configurations.insert({
+  service: "facebook",
+  appId: "324025767759096",
+  secret: "028e31ea7225e27cc7720e6674d7f735"
+});
+
