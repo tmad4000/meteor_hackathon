3cf876caaaa8f6f819147304bb7af8a1712c727d
Brad Ruderman <bradruderman@gmail.com>
2014-07-26 15:58:52 -0700

    Updates

---
+++ client/views/createDebate/createDebate.js
@@ -0,0 +1,14 @@
+Template.createDebate.events = {
+  'click #btnAddDebate': function(e){
+    e.preventDefault();
+    var title = $("#inputTitle").val();
+    var description = $("#inputDescription").val();
+    var url = $("#inputUrl").val();
+    Debates.insert({
+      "title" : title,
+      "description" : description,
+      "url" : url
+    });
+    console.log("yes");
+  }
+}
