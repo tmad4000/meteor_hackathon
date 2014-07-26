5323a773cc16188677908ea00a3b09afa4604286
Kris Hamoud <khamoud@umich.edu>
2014-07-26 16:53:44 -0700

    slug

--- client/views/createDebate/createDebate.js
+++ client/views/createDebate/createDebate.js
@@ -4,10 +4,13 @@ Template.createDebate.events = {
     var title = $("#inputTitle").val();
     var description = $("#inputDescription").val();
     var url = $("#inputUrl").val();
+		var slug = title.split(" ").join("");
+		console.log(slug);
     Debates.insert({
       "title" : title,
       "description" : description,
-      "url" : url
+      "url" : url,
+			"slug" : slug
     });
     Router.go('/');
   }
