247b8c702897552eea1853762ecee6f677a17262
Hyeon Kim <simnalamburt@gmail.com>
2014-07-27 01:43:46 -0700

    Automated slug generation
    1.  Lowercase all characters
    2.  Change all spaces into hypen
    3.  Delete all special characters except hypen

--- client/views/createDebate/createDebate.js
+++ client/views/createDebate/createDebate.js
@@ -1,14 +1,20 @@
 Template.createDebate.events = {
   'click #btnAddDebate': function(e){
     e.preventDefault();
+
     var title = $("#inputTitle").val();
-    var description = $("#inputDescription").val();
-    var url = $("#inputUrl").val();
-    var slug = $("#inputSlug").val();
+    var initSlug = title.toLowerCase().replace(/ /g,'-').replace(/[^a-z0-9\-]/g, '');
+
+    var slug = initSlug;
+    for (var n = 2; Debates.find({slug: slug}).fetch().length > 0; ++n) {
+      slug = initSlug + "-" + n;
+      // #future Will be slow if many slug collisions
+    }
+
     Debates.insert({
       "title" : title,
-      "description" : description,
-      "url" : url,
+      "description" : $("#inputDescription").val(),
+      "url" : $("#inputUrl").val(),
 			"slug" : slug
     });
     Router.go('/debates/' + slug);
