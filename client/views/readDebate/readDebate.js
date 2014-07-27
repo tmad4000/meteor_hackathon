a70e82d7f30ff34112bd4a434ceaa78928aad04b
Brad Ruderman <bradruderman@gmail.com>
2014-07-26 20:43:55 -0700

    t

--- client/views/readDebate/readDebate.js
+++ client/views/readDebate/readDebate.js
@@ -9,9 +9,26 @@ Template.readDebate.events = {
       "votes" : 0,
       "type" : "con"
     }
+  },
+  'click .add-comment' : function(e){
+    Session.set("currentEditing", this._id);
+    $("#testt").focus();
+  },
+  'submit #comment-input' : function(e){
+    e.preventDefault();
+    Points.insert({
+      parent : this._id,
+      text : $("#comment-input").find("input").val()
+    });
+    Session.set("currentEditing", null);
   }
+}
+
+Template.comment.editing = function(){
+  return Session.equals("currentEditing", this._id);
 }
 
 UI.registerHelper("commentHelper" , function(parent){
   return Points.find({"parent": parent})
 });
+
